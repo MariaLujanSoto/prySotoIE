5d64ec9f6b208be72521a0e1651d3ebf74a4a12a
root <mgiraudo@ies21.edu.ar>
2023-09-11 14:29:30 -0300

    Funciones Treeview 11/09

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -28,14 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnAbrirCarpeta = new System.Windows.Forms.Button();
-            this.btnGrabarArchivo = new System.Windows.Forms.Button();
-            this.lblRuta = new System.Windows.Forms.Label();
             this.ventanaCarpetas = new System.Windows.Forms.FolderBrowserDialog();
-            this.uikl = new System.Windows.Forms.TreeView();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnAbrirCarpeta = new System.Windows.Forms.Button();
             this.txtNombreArchivo = new System.Windows.Forms.TextBox();
+            this.lblRuta = new System.Windows.Forms.Label();
+            this.btnGrabarArchivo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(161, 448);
+            this.treeView1.TabIndex = 3;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.uikl_AfterSelect);
+            this.treeView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseDoubleClick);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(216, 1);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(423, 448);
+            this.splitContainer1.SplitterDistance = 141;
+            this.splitContainer1.TabIndex = 7;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(278, 448);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Nombre";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tipo";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Ultima Modificacion";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // btnAbrirCarpeta
             // 
@@ -48,16 +111,12 @@
             this.btnAbrirCarpeta.UseVisualStyleBackColor = true;
             this.btnAbrirCarpeta.Click += new System.EventHandler(this.btnAbrirCarpeta_Click);
             // 
-            // btnGrabarArchivo
+            // txtNombreArchivo
             // 
-            this.btnGrabarArchivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGrabarArchivo.Location = new System.Drawing.Point(13, 265);
-            this.btnGrabarArchivo.Name = "btnGrabarArchivo";
-            this.btnGrabarArchivo.Size = new System.Drawing.Size(187, 37);
-            this.btnGrabarArchivo.TabIndex = 1;
-            this.btnGrabarArchivo.Text = "Grabar Archivo";
-            this.btnGrabarArchivo.UseVisualStyleBackColor = true;
-            this.btnGrabarArchivo.Click += new System.EventHandler(this.btnGrabarArchivo_Click);
+            this.txtNombreArchivo.Location = new System.Drawing.Point(13, 227);
+            this.txtNombreArchivo.Name = "txtNombreArchivo";
+            this.txtNombreArchivo.Size = new System.Drawing.Size(136, 20);
+            this.txtNombreArchivo.TabIndex = 6;
             // 
             // lblRuta
             // 
@@ -68,56 +127,51 @@
             this.lblRuta.TabIndex = 2;
             this.lblRuta.Text = "Ruta...";
             // 
-            // uikl
+            // btnGrabarArchivo
             // 
-            this.uikl.Location = new System.Drawing.Point(248, 27);
-            this.uikl.Name = "uikl";
-            this.uikl.Size = new System.Drawing.Size(161, 210);
-            this.uikl.TabIndex = 3;
+            this.btnGrabarArchivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGrabarArchivo.Location = new System.Drawing.Point(13, 253);
+            this.btnGrabarArchivo.Name = "btnGrabarArchivo";
+            this.btnGrabarArchivo.Size = new System.Drawing.Size(187, 37);
+            this.btnGrabarArchivo.TabIndex = 1;
+            this.btnGrabarArchivo.Text = "Grabar Archivo";
+            this.btnGrabarArchivo.UseVisualStyleBackColor = true;
+            this.btnGrabarArchivo.Click += new System.EventHandler(this.btnGrabarArchivo_Click);
             // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(436, 27);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(181, 369);
-            this.richTextBox1.TabIndex = 5;
-            this.richTextBox1.Text = "";
-            // 
-            // txtNombreArchivo
-            // 
-            this.txtNombreArchivo.Location = new System.Drawing.Point(13, 227);
-            this.txtNombreArchivo.Name = "txtNombreArchivo";
-            this.txtNombreArchivo.Size = new System.Drawing.Size(136, 20);
-            this.txtNombreArchivo.TabIndex = 6;
-            // 
-            // frmNavegar
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(115)))), ((int)(((byte)(159)))));
             this.ClientSize = new System.Drawing.Size(640, 450);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.txtNombreArchivo);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.uikl);
             this.Controls.Add(this.lblRuta);
             this.Controls.Add(this.btnGrabarArchivo);
             this.Controls.Add(this.btnAbrirCarpeta);
-            this.Name = "frmNavegar";
+            this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmNavegar";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnAbrirCarpeta;
-        private System.Windows.Forms.Button btnGrabarArchivo;
-        private System.Windows.Forms.Label lblRuta;
         private System.Windows.Forms.FolderBrowserDialog ventanaCarpetas;
-        private System.Windows.Forms.TreeView uikl;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button btnAbrirCarpeta;
         private System.Windows.Forms.TextBox txtNombreArchivo;
+        private System.Windows.Forms.Label lblRuta;
+        private System.Windows.Forms.Button btnGrabarArchivo;
     }
 }
--- frmMain.cs
+++ frmMain.cs
@@ -17,6 +17,7 @@ namespace prySotoIE
         public frmMain()
         {
             InitializeComponent();
+            PopulateTreeView();
         }
 
         private void btnAbrirCarpeta_Click(object sender, EventArgs e)
@@ -39,5 +40,85 @@ namespace prySotoIE
             MessageBox.Show("Archivo creeado " + txtNombreArchivo.Text);
 
         }
+
+        private void PopulateTreeView()
+        {
+            TreeNode rootNode;
+
+            DirectoryInfo info = new DirectoryInfo(@"../..");
+            if (info.Exists)
+            {
+                rootNode = new TreeNode(info.Name);
+                rootNode.Tag = info;
+                GetDirectories(info.GetDirectories(), rootNode);
+                treeView1.Nodes.Add(rootNode);
+            }
+        }
+
+        private void GetDirectories(DirectoryInfo[] subDirs,
+            TreeNode nodeToAddTo)
+        {
+            TreeNode aNode;
+            DirectoryInfo[] subSubDirs;
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                aNode = new TreeNode(subDir.Name, 0, 0);
+                aNode.Tag = subDir;
+                aNode.ImageKey = "folder";
+                subSubDirs = subDir.GetDirectories();
+                if (subSubDirs.Length != 0)
+                {
+                    GetDirectories(subSubDirs, aNode);
+                }
+                nodeToAddTo.Nodes.Add(aNode);
+            }
+        }
+
+        void treeView1_NodeMouseClick(object sender,
+    TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode newSelected = e.Node;
+            listView1.Items.Clear();
+            DirectoryInfo nodeDirInfo = (DirectoryInfo)newSelected.Tag;
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item = null;
+
+            foreach (DirectoryInfo dir in nodeDirInfo.GetDirectories())
+            {
+                item = new ListViewItem(dir.Name, 0);
+                subItems = new ListViewItem.ListViewSubItem[]
+                    {new ListViewItem.ListViewSubItem(item, "Directory"),
+             new ListViewItem.ListViewSubItem(item,
+                dir.LastAccessTime.ToShortDateString())};
+                item.SubItems.AddRange(subItems);
+                listView1.Items.Add(item);
+            }
+            foreach (FileInfo file in nodeDirInfo.GetFiles())
+            {
+                item = new ListViewItem(file.Name, 1);
+                subItems = new ListViewItem.ListViewSubItem[]
+                    { new ListViewItem.ListViewSubItem(item, "File"),
+             new ListViewItem.ListViewSubItem(item,
+                file.LastAccessTime.ToShortDateString())};
+
+                item.SubItems.AddRange(subItems);
+                listView1.Items.Add(item);
+            }
+
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+      
+
+
+        private void uikl_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void treeView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.treeView1.NodeMouseClick += new TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+        }
     }
 }
+
