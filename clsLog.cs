2af9f415c66767292699bcbcc9fb048e425231ce
root <mgiraudo@ies21.edu.ar>
2023-10-09 14:15:43 -0300

    inicio Acces

---
+++ clsLog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+namespace prySotoIE
+{
+    internal class clsLog
+    {
+        OleDbConnection aperturaBD;
+       //Abrir Acc
+       public void AbrirBD()
+        {
+
+            try
+            {
+                aperturaBD = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0;" + "Data Source = ../../ Resources/Proveedores/BDUsuarios.accdb");
+                aperturaBD.Open();
+            }
+            catch(Exception ex)
+            {
+                //mserror
+            }
+            
+        }
+        //leer BD
+
+        //Validar datos
+
+    }
+}
